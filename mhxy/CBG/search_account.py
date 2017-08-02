da4e7798c7a7cda9c3b5826a26477aca6574afac
clpy9793 <clpy9793@users.noreply.github.com>
2017-08-02 16:20:50 +0800

    .

---
+++ mhxy/CBG/search_account.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-08-02 15:28:21
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os
+import requests
+
+host = 'http://xyq.cbg.163.com'
+uri = '/cgi-bin/xyq_overall_search.py?j5up3b8v&level_min=109&level_max=109&expt_total=68&bb_expt_total=68&skill_qiang_shen=129&skill_shensu=36&skill_qiang_zhuang=36&act=overall_search_role&page=%d'
+equipquery = 'http://xyq.cbg.163.com/cgi-bin/equipquery.py?act=overall_search_show_detail&serverid=127&ordersn=280_1501152627_282072511&equip_refer=10'
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
